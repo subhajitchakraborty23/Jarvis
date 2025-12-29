80667dea43662c53b6d33588655b533246fe7840
subhajitchakraborty23 <subhajit.chakraborty.23@aot.edu.in>
2025-12-29 20:08:42 +0530

    Add new music

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,6 +1,7 @@
 music={"montero":"https://youtu.be/B3Z4XGAxJB0?si=eu3_6xg3-Q6Ii__u",
        "lonely":"https://youtu.be/JISlh7et1w4?si=aiCQJweL6vq3xu5W",
        "taki taki":"https://youtu.be/ixkoVwKQaJg?si=0Nadezueo75qiJsj",
-       "skyfall" : "https://youtu.be/DeumyOzKqgI?si=tl9Tfhos5b07TRsK"
+       "skyfall" : "https://youtu.be/DeumyOzKqgI?si=tl9Tfhos5b07TRsK",
+       "dhurandhar": "https://youtu.be/ZHLOxQWc3P4?si=975FWdhShmNiXB_5"
 
 }
